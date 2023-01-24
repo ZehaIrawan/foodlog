dddf5f5fd0d9fe85d105fcf47bb62b8b312ff7a7
Zeha Irawan <zehairawan@gmail.com>
2023-01-24 20:15:52 +0800

    scaffold entry

---
+++ db/migrate/20230124121228_create_entries.rb
@@ -0,0 +1,12 @@
+class CreateEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entries do |t|
+      t.string :meal_type
+      t.integer :calories
+      t.integer :proteins
+      t.integer :fats
+
+      t.timestamps
+    end
+  end
+end
