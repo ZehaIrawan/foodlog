55b5c2a0db7c8ce17aeca51d08801966dadf854b
Zeha Irawan <zehairawan@gmail.com>
2023-01-27 20:21:15 +0800

    Setup archives controller

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,6 @@
 class Entry < ApplicationRecord
+
+  def day
+    self.created_at.strftime("%b %e, %Y")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'archives/index'
   resources :entries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: "entries#index"
+  
 end
