0a692a147b81344e1b3546a25a74006c4afba764
Zeha Irawan <zehairawan@gmail.com>
2023-01-28 08:13:31 +0800

    add controller and model test

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type:"Breakfast", calories:20, fats:20, proteins:"400")
+    assert entry.save
+  end
 end
